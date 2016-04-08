3e35007f9f9351141071f45a24ebaddf3f55eb4b
Humam Al-Faiz <humam.alfaiz@gmail.com>
2016-04-08 18:15:24 -0400

    Create

--- dbclass2/DataAccess.cs
+++ dbclass2/DataAccess.cs
@@ -138,6 +138,117 @@ namespace dbclass2
             return result;
         }*/
 
+
+        /// <summary>
+        /// Check if Table Exist in DB
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <returns>int</returns>
+        public static int DoesTableExist(string tableName)
+        {
+            List<string> result = new List<string>();
+
+            try
+            {
+                Connect();
+
+                OracleCommand cmd = new OracleCommand();
+
+                cmd.Connection = con;
+
+                cmd.CommandText = "SELECT table_name FROM user_tables Where table_name = '" + tableName + "'";
+
+                OracleDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    result.Add(reader["table_name"].ToString());
+                }
+
+                Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return result.Count;
+        }
+
+        /// <summary>
+        /// Create Dimenstional Table 
+        /// </summary>
+        /// <param name="Table"></param>
+        /// <returns></returns>
+        public static int CreateDimenstionalTable(DimensionalTableInfo Table)
+        {
+            int result = 0;
+
+            string pk = string.Empty;
+
+            Table = new DimensionalTableInfo();
+
+            Table.TableName = "DOC";
+
+            Table.PrimaryKeys = new Dictionary<string, string>();
+
+            Table.PrimaryKeys.Add("PID", "Int");
+
+            Table.Columns = new Dictionary<string, string>();
+
+            Table.Columns.Add("Col1", "Int");
+            Table.Columns.Add("Col2", "Varchar(50)");
+
+            try
+            {
+                if (DoesTableExist(Table.TableName) > 0)
+                    return -99;
+
+                Connect();
+
+                OracleCommand cmd = new OracleCommand();
+
+                cmd.Connection = con;
+
+                StringBuilder sb = new StringBuilder();
+
+                sb.AppendLine(("CREATE TABLE " + Table.TableName));
+
+                sb.AppendLine((" ( "));
+
+                foreach (var item in Table.PrimaryKeys)
+                {
+                    sb.AppendLine(item.Key + " " + item.Value + ",");
+
+                    pk = pk + item.Key + ",";
+                }
+
+                foreach (var item in Table.Columns)
+                {
+                    sb.AppendLine(item.Key + " " + item.Value + ",");
+                }
+
+                pk = pk.TrimEnd(',');
+
+                sb.AppendLine(" CONSTRAINT " + Table.TableName + "_PK PRIMARY KEY (" + pk + ") ");
+
+                sb.AppendLine((")"));
+
+                cmd.CommandText = sb.ToString();
+
+                result = cmd.ExecuteNonQuery();
+
+                Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return result;
+        }
+
+
+
         public static void Close()
         {
             con.Close();
